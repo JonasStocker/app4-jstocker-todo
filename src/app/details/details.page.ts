ff1029e5a12c23f20c3fa1f91c8587e77e2fec87
Jonas Stocker <jstocker@schueler.hakzell.at>
2020-04-22 15:09:32 +0200

    AA 22.04.2020

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  buttonText = "Zurück";
+  content = "";
+  title: "";
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.title = this.router.getCurrentNavigation().extras.state.title,
+        this.content = this.router.getCurrentNavigation().extras.state.content
+      }
+  });
+}
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { v4 } from 'uuid';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +10,43 @@ import { Component } from '@angular/core';
 
 export class HomePage {
 
+  todo = [
+    {id: v4(), title: 'Arbeitsauftrag erledigen', note: 'Arbeitsauftrag muss noch erledigt werden', priority: 1, due: '2020-03-25', done: false},
+    {id: v4(), title: 'Arbeitsauftrag in APP4', note: 'Arbeitsauftrag in APP4 wurde schon erledigt', priority: 2, due: '2020-03-18', done: true}
+  ];
+
   title = 'Aufgaben';
 
-  todo = [];
+  constructor(private router: Router) {
+  }
 
-  constructor() {
-    this.todo = [
-      {title: 'Arbeitsauftrag erledigen', priority: 1, due: '2020-03-25', done: false},
-      {title: 'Arbeitsauftrag in APP4', priority: 2, due: '2020-03-18', done: true}
-    ];
+  ionViewWillEnter() {
+    let id = v4();
+    console.log(this.todo);
+  }
+
+  showNote(id) {
+    const pos = this.todo.findIndex((t) => {
+      return t.id == id;
+    });
+
+    console.log(pos);
+
+    if (pos > -1) {
+      const title = this.todo[pos].title;
+      
+      console.log("Titel: " + title);
+
+      let extras: NavigationExtras = {
+        state: {
+          id: id,
+          title: title,
+          content: this.todo[pos].note
+        }
+      };
+
+      this.router.navigate(['/details'], extras);
+    }
   }
 
   setDone(item){
